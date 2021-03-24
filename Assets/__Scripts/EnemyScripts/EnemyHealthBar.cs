9a5e6af090b1d5c74373e5bae79c89ab0258794d
danalkwifi <71096802+danalkwifi@users.noreply.github.com>
2021-03-24 02:50:56 -0400

    Dana

---
+++ Assets/__Scripts/EnemyScripts/EnemyHealthBar.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealthBar : EnemyHealth
+{
+    void TakeDamage(int damage)
+    {
+  
+        health -= damage;
+    }
+  
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Bullet")
+        {
+            TakeDamage(30);
+        }
+
+        if (collision.gameObject.tag == "Player" && PlayerKnife.isHitting == true)
+        {
+            TakeDamage(20);
+        }
+    }
+}
